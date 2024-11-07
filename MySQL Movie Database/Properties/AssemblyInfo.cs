cfd767b2a89fe163b070a877c7590609fe40daf6
Danial-Changez <86434442+Danial-Changez@users.noreply.github.com>
2024-11-06 19:08:15 -0500

    Update AssemblyInfo.cs

--- MySQL Movie Database/Properties/AssemblyInfo.cs
+++ MySQL Movie Database/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Culminating Movie Database")]
+[assembly: AssemblyTitle("MySQL Movie Database")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Culminating Movie Database")]
+[assembly: AssemblyProduct("MySQL Movie Database")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
